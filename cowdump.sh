9eca12961103a255c8cdfc862ed8235a732e8279
lucas <lucasvlopez@gmail.com>
2025-10-31 17:52:32 +0100

    cow not working

--- cowdump.sh
+++ cowdump.sh
@@ -1,6 +1,7 @@
 #!/bin/bash 
 
-filepath='/var/www/gengiswlan.xyz/articles/dumpfile'
+filepath='/var/www/gengiswlan.xyz/articles/dumpfile.html'
 > $filepath
+echo "<!DOCTYPE html>" >>$filepath
 fortune -a | cowsay >> $filepath
 exit
